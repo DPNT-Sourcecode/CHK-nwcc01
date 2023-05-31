a82d6a2309927ae17de89f4dc311d606f8ae4925
sbx_user1051 <sbx_user1051@169.254.12.93>
2023-05-31 14:35:30 +0000

    Wed May 31 14:35:30 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,19 +3,48 @@ from typing import Union
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+
 def checkout(skus: str) -> int:
+    supermarket_item_dict = parse_string(skus)
 
-    """- param[0] = a String containing the SKUs of all the products in the basket
-     - @return = an Integer representing the total checkout value of the items"""
-
-    # not sure what the input data looks like so need to deploy to find out
-
-    # refactor
-
-
+    # invalid string return -1
+    if supermarket_item_dict == -1:
+        return -1
 
     # calculate the cost of A items
+    total_cost_a = calcualte_cost_of_a_item(supermarket_item_dict)
+    # calculate the cost of B items
+    total_cost_b = calculate_cost_of_b_item(supermarket_item_dict)
+    # Calculate cost of C items
+    total_cost_c = calculate_cost_c_item(supermarket_item_dict)
+    # Calculate cost of D items
+    total_cost_d = calculate_cost_d_item(supermarket_item_dict)
 
+    total_cost = total_cost_a + total_cost_b + total_cost_c + total_cost_d
+
+    return total_cost
+
+
+def parse_string(skus: str) -> Union[int, dict]:
+    """ Store number of each item in a dictionary"""
+    supermarket_item_dict = {
+        "A": 0,
+        "B": 0,
+        "C": 0,
+        "D": 0
+    }
+    # loop through the string to determine how many of each item is required and return -1 if an invalid input is given
+    for char in skus:
+        if char != "A" and char != "B" and char != "C" and char != "D":
+            return -1
+        else:
+            supermarket_item_dict[char] += 1
+
+    return supermarket_item_dict
+
+
+def calcualte_cost_of_a_item(supermarket_item_dict: dict) -> int:
     quantity_a = supermarket_item_dict["A"]
     cost_a_single = 50
     bulk_price_a = 130
@@ -33,8 +62,10 @@ def checkout(skus: str) -> int:
     else:
         total_cost_a = quantity_a * cost_a_single
 
-    # calculate the cost of B items
+    return total_cost_a
 
+
+def calculate_cost_of_b_item(supermarket_item_dict: dict) -> int:
     quantity_b = supermarket_item_dict["B"]
     cost_b_single = 30
     bulk_price_b = 45
@@ -52,38 +83,22 @@ def checkout(skus: str) -> int:
     else:
         total_cost_b = quantity_b * cost_b_single
 
+    return total_cost_b
 
-    # Calculate cost of C items
 
+def calculate_cost_c_item(supermarket_item_dict: dict) -> int:
     quantity_c = supermarket_item_dict["C"]
     cost_c_single = 20
 
     total_cost_c = quantity_c * cost_c_single
 
-    # Calculate cost of D items
+    return total_cost_c
 
+
+def calculate_cost_d_item(supermarket_item_dict: dict) -> int:
     quantity_d = supermarket_item_dict["D"]
     cost_d_single = 15
-
     total_cost_d = quantity_d * cost_d_single
 
-    total_cost = total_cost_a + total_cost_b + total_cost_c + total_cost_d
+    return total_cost_d
 
-    return total_cost
-
-
-def parse_string(skus: str) -> Union[]:
-    supermarket_item_dict = {
-        "A": 0,
-        "B": 0,
-        "C": 0,
-        "D": 0
-    }
-    # loop through the string to determine how many of each item is required and return -1 if an invalid input is given
-    for char in skus:
-        if char != "A" and char != "B" and char != "C" and char != "D":
-            return -1
-        else:
-            supermarket_item_dict[char] += 1
-
-    return supermarket_item_dict
