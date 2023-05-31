ef09b4ee5526ea18208a7f4b329edfbb7664f972
sbx_user1051 <sbx_user1051@169.254.12.93>
2023-05-31 19:21:00 +0000

    Wed May 31 19:21:00 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,6 +5,36 @@ from typing import Union
 # skus = unicode string
 
 
+supermarket_item_dict = {
+        "E": [0, 40, "free deal 1", [2, "B"]],
+        "N": [0, 40, "free deal 1", [3, "M"]],
+        "R": [0, 50, "free deal 1", [3, "Q"]],
+        "A": [0, 50, "complex deal", [3, 130], [5, 200]],
+        "B": [0, 30, "simple deal", [2, 45]],
+        "C": [0, 20, "single"],
+        "D": [0, 15, "single"],
+        "F": [0, 10, "free deal 2", 3],
+        "G": [0, 20, "single"],
+        "H": [0, 10, "complex deal", [5, 45], [10, 80]],
+        "I": [0, 35, "single"],
+        "J": [0, 60, "single"],
+        "K": [0, 80, "simple deal", [2, 150]],
+        "L": [0, 90, "single"],
+        "M": [0, 15, "single"],
+        "O": [0, 10, "single"],
+        "P": [0, 50, "simple deal", [5, 200]],
+        "Q": [0, 30, "simple deal", [3, 80]],
+        "S": [0, 20, "group deal"],
+        "T": [0, 20, "group deal"],
+        "U": [0, 40, "free deal 2", 4],
+        "V": [0, 50, "complex deal", [2, 90], [3, 130]],
+        "W": [0, 20, "single"],
+        "X": [0, 17, "group deal"],
+        "Y": [0, 20, "group deal"],
+        "Z": [0, 21, "group deal"]
+    }
+
+
 def checkout(skus: str) -> int:
     supermarket_item_dict = parse_string(skus)
 
@@ -77,45 +107,16 @@ def checkout(skus: str) -> int:
             data = supermarket_item_dict.get(product_name)
             group_deal_dict[product_name] = data[0]
 
-    # group_deal_quantity_calculation(group_deal_dict, )
-
-
-    total_cost = complex_deal_total + simple_deal_total + easy_ones_total + free_deal_1_total + free_deal_2_total
+    group_deal_total = group_deal_quantity_calculation(group_deal_dict)
+    total_cost = complex_deal_total + simple_deal_total + easy_ones_total + free_deal_1_total + free_deal_2_total \
+                 + group_deal_total
 
     return total_cost
 
 
 def parse_string(skus: str) -> Union[int, dict]:
     """ Store number of each item in a dictionary"""
-    supermarket_item_dict = {
-        "E": [0, 40, "free deal 1", [2, "B"]],
-        "N": [0, 40, "free deal 1", [3, "M"]],
-        "R": [0, 50, "free deal 1", [3, "Q"]],
-        "A": [0, 50, "complex deal", [3, 130], [5, 200]],
-        "B": [0, 30, "simple deal", [2, 45]],
-        "C": [0, 20, "single"],
-        "D": [0, 15, "single"],
-        "F": [0, 10, "free deal 2", 3],
-        "G": [0, 20, "single"],
-        "H": [0, 10, "complex deal", [5, 45], [10, 80]],
-        "I": [0, 35, "single"],
-        "J": [0, 60, "single"],
-        "K": [0, 80, "simple deal", [2, 150]],
-        "L": [0, 90, "single"],
-        "M": [0, 15, "single"],
-        "O": [0, 10, "single"],
-        "P": [0, 50, "simple deal", [5, 200]],
-        "Q": [0, 30, "simple deal", [3, 80]],
-        "S": [0, 20, "group deal"],
-        "T": [0, 20, "group deal"],
-        "U": [0, 40, "free deal 2", 4],
-        "V": [0, 50, "complex deal", [2, 90], [3, 130]],
-        "W": [0, 20, "single"],
-        "X": [0, 17, "group deal"],
-        "Y": [0, 20, "group deal"],
-        "Z": [0, 21, "group deal"]
 
-    }
     # loop through the string to determine how many of each item is required and return -1 if an invalid input is given
     for char in skus:
         quantity = supermarket_item_dict.get(char, None)
@@ -209,8 +210,7 @@ def complex_deal_calculation(product_price: int, small_product_deal_price: int, 
     return total_cost
 
 
-def group_deal_quantity_calculation(group_deal_dict: dict, s_cost: int, t_cost: int, x_cost: int, y_cost: int,
-                                    z_cost: int) -> int:
+def group_deal_quantity_calculation(group_deal_dict: dict) -> int:
     deal_quantity = 3
     deal_cost = 45
     total_quantity = sum(group_deal_dict.values())
@@ -234,21 +234,21 @@ def group_deal_quantity_calculation(group_deal_dict: dict, s_cost: int, t_cost: 
                     total_quantity - group_deal_dict[product]
 
         total_cost = (math.floor(total_quantity / deal_quantity) * deal_cost) + group_deal_remainder_cost_calculation(
-            group_deal_dict, s_cost, t_cost, x_cost, y_cost, z_cost)
+            group_deal_dict)
 
     return total_cost
 
 
-def group_deal_remainder_cost_calculation(group_deal_dict: dict, s_cost: int, t_cost: int, x_cost: int, y_cost: int,
-                                    z_cost: int) -> int:
+def group_deal_remainder_cost_calculation(group_deal_dict: dict) -> int:
 
-    s_total = group_deal_dict.get("S") * s_cost
-    t_total = group_deal_dict.get("T") * t_cost
-    x_total = group_deal_dict.get("X") * x_cost
-    y_total = group_deal_dict.get("Y") * y_cost
-    z_total = group_deal_dict.get("Z") * z_cost
+    s_total = group_deal_dict.get("S") * supermarket_item_dict.get("S")[1]
+    t_total = group_deal_dict.get("T") * supermarket_item_dict.get("T")[1]
+    x_total = group_deal_dict.get("X") * supermarket_item_dict.get("X")[1]
+    y_total = group_deal_dict.get("Y") * supermarket_item_dict.get("Y")[1]
+    z_total = group_deal_dict.get("Z") * supermarket_item_dict.get("Z")[1]
 
     total_cost = sum([s_total, t_total, x_total, y_total, z_total])
 
     return total_cost
+
 
