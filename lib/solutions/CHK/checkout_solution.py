3bbd6128b2ef1b44cf2f70d09cba726139614fce
sbx_user1051 <sbx_user1051@169.254.12.93>
2023-05-31 13:42:55 +0000

    Wed May 31 13:42:55 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,7 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str) -> int:
+
+    # we are getting a string with all of
+
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,8 +1,13 @@
 import unittest
+from lib.solutions.CHK import checkout_solution
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+class SupermarketCheckOutExercise(unittest.TestCase):
+    def test_checkout_method(self):
+
+        self.assertEqual(checkout_solution.checkout(5), 10)
+
 
 if __name__ == '__main__':
     unittest.main()
+
