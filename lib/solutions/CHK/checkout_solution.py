11a356c32e7c1ac31227a11d8d8aee75eec74619
sbx_user1051 <sbx_user1051@169.254.12.93>
2023-05-31 13:44:30 +0000

    Wed May 31 13:44:30 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,5 +4,11 @@
 # skus = unicode string
 def checkout(skus: str) -> int:
 
-    # we are getting a string with all of
+    """- param[0] = a String containing the SKUs of all the products in the basket
+     - @return = an Integer representing the total checkout value of the items"""
+
+    # not sure what the input data looks like so need to deploy to find out
+
+    return 0
+
 
