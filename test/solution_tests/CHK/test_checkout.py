eff1029117a43fe9e4e8da21774075f5d32e283c
sbx_user1051 <sbx_user1051@169.254.12.93>
2023-05-31 14:14:30 +0000

    Wed May 31 14:14:30 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,11 +3,20 @@ from lib.solutions.CHK import checkout_solution
 
 
 class SupermarketCheckOutExercise(unittest.TestCase):
-    def test_checkout_method_when_invalid_input_given(self):
+    def test_returns_minus_one_when_invalid_string_is_given(self):
 
         self.assertEqual(checkout_solution.checkout("Invalid String"), -1)
+
+    def test_checkout_method_returns_minus_one_when_invalid_characters_part_of_string(self):
+        test_string = "ABCxD"
+        self.assertEqual(checkout_solution.checkout(test_string), -1)
+
+    def test_checkout_method_returns_correct_calculation_for_one_of_each_item(self):
+        test_string = "ABCD"
+        self.assertEqual(checkout_solution.checkout(test_string), 115)
 
 
 if __name__ == '__main__':
     unittest.main()
+
 
